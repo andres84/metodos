20a194297a698136610b78baf2da368cece6567b
sara <sara@localhost>
2016-08-28 01:55:01 -0500

    practica 3 ejemplo metodos de clase y de instancias

---
+++ metodos/src/metodosInstanciaClase/Persona.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodosInstanciaClase;
+
+/**
+ *
+ * @author sara
+ */
+public class Persona {
+    
+    private String nombre;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public void mostrarDeInstancia(){
+        
+        System.out.println(this.nombre + " " + this.edad);
+        
+    }
+    
+    public static void mostrarDeClase(){
+        
+        System.out.println("metodo estatico");
+        
+    }
+    
+}
---
+++ metodos/src/metodosInstanciaClase/app.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodosInstanciaClase;
+
+/**
+ *
+ * @author sara
+ */
+public class app {
+    
+    public static void main(String... andres){
+        
+        Persona per = new Persona();
+        per.setNombre("andres");
+        per.setEdad(32);
+        per.mostrarDeInstancia();
+        
+        Persona.mostrarDeClase();
+        
+    }
+    
+}
